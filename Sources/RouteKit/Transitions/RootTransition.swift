7821a0ff522ec349061f92abb4eda5a5b8cbb02f
Oleksii Zghurskyi <mr.noone@icloud.com>
2021-12-07 14:02:34 +0200

    Add root transition

---
+++ Sources/RouteKit/Transitions/RootTransition.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+public final class RootTransition: NSObject, PresentableTransition {
+  // MARK: - Properties
+  
+  private weak var window: UIWindow?
+  private let options: UIView.AnimationOptions
+  private let duration: TimeInterval
+  
+  // MARK: - Inits
+  
+  public init(window: UIWindow,
+              options: UIView.AnimationOptions = .transitionCrossDissolve,
+              duration: TimeInterval = 0.2) {
+    self.window = window
+    self.options = options
+    self.duration = duration
+  }
+  
+  // MARK: - PresentableTransition
+  
+  public func present(_ viewController: UIViewController, animated: Bool, completion: Completion?) {
+    guard let window = window else { return }
+    window.rootViewController = viewController
+    if animated {
+      UIView.transition(
+        with: window,
+        duration: duration,
+        options: options,
+        animations: nil
+      ) { _ in
+        completion?()
+      }
+    } else {
+      completion?()
+    }
+  }
+}
